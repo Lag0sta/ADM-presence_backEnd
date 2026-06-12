7ef5e05fd9dfe106ac3cfbcefcc65ecd592015bf
LRonnet <leo.ronnet@gmail.com>
2026-06-12 17:42:40 +0200

    test 5

--- api/index.ts
+++ api/index.ts
@@ -1,29 +1,33 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import serverless from 'serverless-http';
 import app from '../src/app';
 import { connectToDatabase } from '../src/mongodb';
 
 const uri = process.env.CONNECTION_STRING || '';
 
-// On connecte la DB une seule fois au premier appel, 
-// pour éviter de reconnecter à chaque invocation serverless
-
-let isConnected = false;
-
 const handler = async (req: any, res: any) => {
-  if (!isConnected) {
-    try {
-      await connectToDatabase(uri);
-      isConnected = true;
-    } catch (error) {
-      console.error('❌ Erreur lors de la connexion à la DB dans Vercel', error);
-      res.status(500).send('Erreur serveur');
-      return;
-    }
+  try {
+    console.time('mongo');
+
+    await connectToDatabase(uri);
+
+    console.timeEnd('mongo');
+
+
+    console.time('request');
+
+    const result = await app(req, res);
+
+    console.timeEnd('request');
+
+    return result;
+
+  } catch (error) {
+    console.error('❌ Erreur lors de la connexion à la DB dans Vercel', error);
+
+    return res.status(500).send('Erreur serveur');
   }
-  return app(req, res);
 };
 
 export default handler;
--- src/mongodb.ts
+++ src/mongodb.ts
@@ -1,18 +1,24 @@
 import mongoose from 'mongoose';
 
 export async function connectToDatabase(uri: string) {
-  if (mongoose.connection.readyState === 1) {
-    console.log('✅ MongoDB déjà connecté (Mongoose)');
+  if (mongoose.connection.readyState >= 1) {
     return;
   }
+
   if (!uri) {
     throw new Error('La chaîne de connexion MongoDB est vide');
   }
+
   try {
-    await mongoose.connect(uri, {connectTimeoutMS: 2000});
-    console.log('✅ MongoDB connecté avec Mongoose');
+    await mongoose.connect(uri, {
+      connectTimeoutMS: 2000,
+    });
+
+    console.log('✅ MongoDB connecté');
+
   } catch (error) {
     console.error('❌ Erreur connexion MongoDB:', error);
+
     throw error;
   }
 }
