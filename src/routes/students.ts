f3aae9fa32835ff3691bcc6bf0fa01817a717bd6
LRonnet <leo.ronnet@gmail.com>
2026-08-14 02:40:05 +0200

    update

--- src/routes/students.ts
+++ src/routes/students.ts
@@ -10,7 +10,7 @@ const router = Router();
 router.get("/", async (req, res) => {
     try {
         const registrants = await Student.find().select(
-            "_id apellido name age_group subscription endDate pointsLeft payementStatus amount2Pay isAdmin"
+            "_id apellido name age_Group subscription endDate pointsLeft payementStatus amount2Pay isAdmin"
         );;
         res.json({ result: true, data: registrants });
 
