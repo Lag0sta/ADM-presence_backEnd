b3554206bb9da9f442ad60878647a98eeee5d836
LRonnet <leo.ronnet@gmail.com>
2026-08-18 16:50:22 +0200

    vérification valeurs

--- src/routes/students.ts
+++ src/routes/students.ts
@@ -171,7 +171,7 @@ router.put("/updateCardSubscription", validate(updateCardSubscriptionSchema), as
 router.put("/updateStudentFile", validate(updateStudentFileSchema), async (req, res) => {
     try {
         const { studentId, token, updateData } = req.body;
-
+        console.log(typeof updateData.student.subscription.pointsLeft);
         const isAdmin = await Student.findOne({ token });
 
         if (!isAdmin) return res.status(403).json({ result: false, message: "Accès réservé aux administrateurs" });
