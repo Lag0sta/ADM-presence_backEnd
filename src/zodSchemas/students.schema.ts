6cd84cd79680a5dfa4b6638d27c8c67b565935da
LRonnet <leo.ronnet@gmail.com>
2026-08-18 17:48:36 +0200

    updated Zod

--- src/zodSchemas/students.schema.ts
+++ src/zodSchemas/students.schema.ts
@@ -15,32 +15,61 @@ export const updateCardSubscriptionSchema = z.object({
 })
 
 export const updateStudentFileSchema = z.object({
-    studentId: z.string({ message: "L'id de l'élève est obligatoire et doit être une chaîne de caractères" }).regex(objectIdRegex, { message: "L'id de l'élève doit être un ObjectId valide" }),
-    token: z.string({ message: "Le token est obligatoire et doit être une chaîne de caractères", }).min(1, { message: "Le token ne peut pas être vide" }),
-    updateData: z.object({
-        admin: z.object({
-            subscription: z.object({
-                plan: z.enum(["trimestriel", "carte", "annuel"], { message: "Le type d'abonnement doit être 'trimestriel' ou 'carte'", }).optional(),
-                startDate: z.preprocess((value) => value === "" ? undefined : value, z.coerce.date({ message: "La date de début n'est pas valide", }).optional()),
-                endDate: z.preprocess((value) => value === "" ? undefined : value, z.coerce.date({ message: "La date de fin n'est pas valide", }).optional()),
-                pointsLeft: z.number({ message: "Le nombre de points doit être un nombre", }).optional(),
-                amount2Pay: z.number({ message: "Le montant à payer doit être un nombre ", }).optional(),
-            }).optional(),
-        }).optional(),
-        student: z.object({
-            apellido: z.string({ message: "Le apellido doit être une chaîne de caractères", }).optional(),
-            name: z.string({ message: "Le nom doit être une chaîne de caractères", }).optional(),
-            age_Group: z.string({ message: "Le groupe d'age doit être une chaîne de caractères", }).optional(),
-            subscription: z.object({
-                plan: z.enum(["trimestriel", "carte"], { message: "Le type d'abonnement doit être 'trimestriel' ou 'carte'", }).optional(),
-                startDate: z.preprocess((value) => value === "" ? undefined : value, z.coerce.date({ message: "La date de début n'est pas valide", }).optional()),
-                endDate: z.preprocess((value) => value === "" ? undefined : value, z.coerce.date({ message: "La date de fin n'est pas valide", }).optional()),
-                pointsLeft: z.number({ message: "Le nombre de points doit être un nombre", }).optional(),
-                amount2Pay: z.number({ message: "Le montant à payer doit être un nombre", }).optional(),
-            }).optional(),
-        }).optional(),
-    }),
-})
+    studentId: z.string({ message: "L'id de l'élève est obligatoire et doit être une chaîne de caractères", }).regex(objectIdRegex, { message: "L'id de l'élève doit être un ObjectId valide", }),
+    token: z.string({ message: "Le token est obligatoire et doit être une chaîne de caractères", }).min(1, { message: "Le token ne peut pas être vide", }),
+
+    updateData: z.union([
+        z.object({
+            admin: z.object({
+                subscription: z.object({
+                    plan: z.enum(["trimestriel", "carte", "annuel"], { message: "Le type d'abonnement n'est pas valide", }).optional(),
+
+                    startDate: z.preprocess(
+                        (value) => (value === "" ? undefined : value),
+                        z.coerce.date({ message: "La date de début n'est pas valide", }).optional()),
+
+                    endDate: z.preprocess(
+                        (value) => (value === "" ? undefined : value),
+                        z.coerce.date({ message: "La date de fin n'est pas valide", }).optional()),
+
+                    pointsLeft: z.number({ message: "Le nombre de points doit être un nombre", }).optional(),
+
+                    amount2Pay: z.number({ message: "Le montant à payer doit être un nombre", }).optional(),
+                }).optional(),
+            }),
+        }),
+
+        z.object({
+            student: z.object({
+                apellido: z.string({ message: "Le apellido doit être une chaîne de caractères", }).optional(),
+
+                name: z
+                    .string({ message: "Le nom doit être une chaîne de caractères", }).optional(),
+
+                age_Group: z
+                    .string({ message: "Le groupe d'âge doit être une chaîne de caractères", }).optional(),
+
+                subscription: z
+                    .object({
+                        plan: z.enum(["trimestriel", "carte"], { message: "Le type d'abonnement n'est pas valide", }).optional(),
+
+                        startDate: z.preprocess(
+                            (value) => (value === "" ? undefined : value),
+                            z.coerce.date({ message: "La date de début n'est pas valide", }).optional()),
+
+                        endDate: z.preprocess(
+                            (value) => (value === "" ? undefined : value),
+                            z.coerce.date({ message: "La date de fin n'est pas valide", }).optional()),
+
+                        pointsLeft: z.number({ message: "Le nombre de points doit être un nombre", }).optional(),
+
+                        amount2Pay: z.number({ message: "Le montant à payer doit être un nombre", }).optional(),
+                    })
+                    .optional(),
+            }),
+        }),
+    ]),
+});
 
 export const newSubscriptionSchema = z.object({
     studentId: z.string().regex(objectIdRegex, { message: "L'id de l'élève doit être un ObjectId valide" }),
